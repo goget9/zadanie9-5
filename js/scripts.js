53d96892261d6ae1c81a62c2054fff19869afc39
Woji <lubowicki.w@gmail.com>
2018-01-17 22:57:01 +0100

    task finished

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// scripts.js
+
+var withButtonItemClass = document.getElementsByClassName('button');
+
+console.log(withButtonItemClass);
+
+for ( var i = 0 ; i < withButtonItemClass.length ; i++ ) {
+
+	console.log(withButtonItemClass[i].innerText);
+}
